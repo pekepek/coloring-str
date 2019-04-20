b1091d4c06ed1dd883bbb3fd27cc32c254bbe337
pekepek <ishihata@33i.co.jp>
2019-04-20 12:53:07 +0900

    build gem template

---
+++ lib/coloring/str.rb
@@ -0,0 +1,8 @@
+require "coloring/str/version"
+
+module Coloring
+  module Str
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/coloring/str/version.rb
@@ -0,0 +1,5 @@
+module Coloring
+  module Str
+    VERSION = "0.1.0"
+  end
+end
