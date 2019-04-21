f716f16725fd90d1481678835dc46747b175504b
pekepek <ishihata@33i.co.jp>
2019-04-21 21:09:48 +0900

    add TODO comments

--- lib/coloring/str.rb
+++ lib/coloring/str.rb
@@ -2,3 +2,15 @@ require 'coloring/str/version'
 require 'coloring/str/character'
 require 'coloring/str/editor'
 require 'io/console'
+
+# TODO
+# 色変える場合も入力されちゃうのどうにかする
+# 色入力中に deleet 押すと上手く動かない
+# 色入力して Enter 押すと次の行に移っちゃう
+# 入力結果を保存出来るようにする
+# 最終的には Qiita の形式で保存出来るようにする
+# たまにバグでカーソル移動中に文字列が入る
+# 文字列が上書きされる
+# リファクタリング
+# Gem の内容全然変わったから名前変える
+# エディタの表示範囲
